968eb4d5de2c201ca290ea98258a458aebac781a
akhiltest <akhilvijayan91@gmail.com>
2020-07-18 10:50:05 +0530

    comit

--- Akhil_DataDrivenFrameWork/src/main/java/baseclass/driverinstance.java
+++ Akhil_DataDrivenFrameWork/src/main/java/baseclass/driverinstance.java
@@ -5,12 +5,14 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import utilityPackage.ExtentUtils;
 
 
